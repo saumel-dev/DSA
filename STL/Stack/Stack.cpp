c0dc1d2ddf7e6303eacd3f9ecafd6dd2166a16d6
Saumel-Das <saumel.spr@gmail.com>
2026-08-12 12:13:28 +0600

    covered stack

---
+++ STL/Stack/Stack.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    stack<int> st;
+    st.push(1); // Pushes the value 1 onto the stack
+    st.push(2); // Pushes the value 2 onto the stack
+    // time complexity of push() is O(1) in average case and O(1) in worst case.
+    cout << st.top() << endl; // Output: 2, returns the top element
+
+    st.pop(); // Removes the top element from the stack
+    // time complexity of pop() is O(1) in average case and O(1) in worst case.
+
+    cout << st.top() << endl; // Output: 1, returns the top element
+}
